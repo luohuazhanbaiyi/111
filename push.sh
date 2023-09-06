c4c18833f27783e23943f48347a21bd4f8e3ff4e
luohuazhanbaiyi <2937151963@qq.com>
2023-09-06 20:26:52 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'luohuazhanbaiyi'
 git config --global user.email '2937151963@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m '自动推送'
 git push
